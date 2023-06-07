5b3d0f0765702b17d15970572a6f75dde671c861
yeganehRajab <rajabi.yeganeh@yahoo.com>
2023-06-07 14:25:05 +0430

    first commit

---
+++ pages/home/index.jsx
@@ -0,0 +1,15 @@
+import { Button, Stack } from "@mui/material";
+import style from "../../src/components/pages/home/homePage.styles";
+
+const HomePage = () => {
+  return(
+    <Stack justifyContent={'center'} alignItems={'center'} spacing={2}>
+      <Button variant="contained" sx={style.ButtonSx()}>Contained</Button>
+      <Button variant="outlined" sx={style.ButtonSx()}>Contained</Button>
+      <Button variant="contained" sx={style.ButtonSx()}>Contained</Button>
+
+    </Stack>
+  )
+};
+
+export default HomePage;
---
+++ src/components/pages/home/homePage.styles.js
@@ -0,0 +1,14 @@
+const stackSx = () => ({
+  color: "red",
+});
+
+const ButtonSx = () => ({
+    width:'50%'
+  });
+
+const homePageStyles = {
+  stackSx,
+  ButtonSx
+};
+
+export default homePageStyles;
